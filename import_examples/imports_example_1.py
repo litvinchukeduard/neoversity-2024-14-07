6169edec43edaa4b97be331092b5dc44ab52ac5f
Eduard Litvinchuck <litvinchukeduard@gmail.com>
2024-07-14 16:56:56 +0300

    Imports example

---
+++ import_examples/imports_example_1.py
@@ -0,0 +1,22 @@
+import re
+
+# re.search()
+
+import sys
+
+sys.argv
+
+import my_useful_module
+
+# my_useful_module.print_hello()
+
+from datetime import date
+
+# datetime.date
+# date
+
+from my_useful_module import print_hello
+
+# print_hello()
+
+print(f"example 1: {__name__}")
---
+++ import_examples/my_useful_module.py
@@ -0,0 +1,14 @@
+
+my_secret_code = 'my secret code'
+
+def print_hello():
+    print("Hello")
+
+def print_goodbye():
+    print("Good bye!")
+
+# print(dir("Hello"))
+print(__name__)
+
+if __name__ == '__main__': # my_useful_module
+    print_hello()
