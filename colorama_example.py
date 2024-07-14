80d2e3373aadb97548c2a0b116dc7ca45ca4fced
Eduard Litvinchuck <litvinchukeduard@gmail.com>
2024-07-14 15:57:08 +0300

    Colorama examples

---
+++ colorama_example.py
@@ -0,0 +1,14 @@
+from colorama import Fore, Back, Style
+
+# print(Fore.RED + 'some red text')
+# print(Back.GREEN + 'and with a green background')
+# print(Style.DIM + 'and in dim text')
+# print(Style.RESET_ALL)
+# print('back to normal now')
+
+# print(Fore.RED + Back.BLUE + "my colored text" + Style.RESET_ALL)
+# print("Hello!")
+
+my_red = Fore.RED
+
+print(my_red + "my red text")
---
+++ colorama_example2.py
@@ -0,0 +1,6 @@
+from colorama import Fore, Back, Style, init
+
+init(autoreset=True)
+print(Fore.RED + 'some red text')
+print(Fore.BLUE + 'blue text')
+print("regular text")
